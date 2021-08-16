8086a5f8256ae40ed1feafeec6175e26563c74f4
oguzhanakdogan <oguzhnakdogan06@gmail.com>
2021-08-16 18:58:28 +0300

    Initial commit

---
+++ RestfulAPI/RestfulAPI/DAL/HotelDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestfulAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestfulAPI.DAL
+{
+    public class HotelDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Server=ELD-PC-23\SQLEXPRESS; Database=HotelDB; uid=sa; pwd=Password123");
+        }
+
+        public DbSet<Hotel> Hotels {get; set;}
+
+    }
+}
---
+++ RestfulAPI/RestfulAPI/Models/Hotel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestfulAPI.Models
+{
+    
+    public class Hotel
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+    
+         [StringLength(50)]
+        public string Name { get; set; }
+    
+        [StringLength(50)]
+        public string City { get; set; }
+    }
+}
+
